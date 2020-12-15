b257f57674b9648dcb6ab4bbc706a9188c95f477
lunajupiter <jupiter.77.pito@gmail.com>
2020-12-16 08:01:14 +0900

    Initial Commit

---
+++ classes/model/UserModel.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * ユーザーモデル
+ */
+final class UserModel
+{
+    private $_userId = null;
+    private $_password = null;
+    private $_displayName = null;
+    private $_email = null;
+    private $_token = null;
+    private $_loginFailureCount = null;
+    private $_loginFailureDatetime = null;
+    private $_deleteFlag = null;
+}
